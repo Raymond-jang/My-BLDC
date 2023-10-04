2d91fc16d257b097477d7af1edc006aec10f869b
Raymond-jang <raymond7.jang@gmail.com>
2023-10-03 21:25:37 -0500

    UART

---
+++ P01_LED_UART2/Objects/main.d
@@ -0,0 +1,24 @@
+./objects/main.o: app\main.c RTE\Device\TLE9879QXA40\tle_device.h \
+  RTE\Device\TLE9879QXA40\tle_variants.h \
+  RTE\Device\TLE9879QXA40\tle987x.h \
+  C:\Users\polyn\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\polyn\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\polyn\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\polyn\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  RTE\Device\TLE9879QXA40\system_tle987x.h \
+  RTE\Device\TLE9879QXA40\types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  RTE\_TLE9879_EvalKit\RTE_Components.h \
+  RTE\Device\TLE9879QXA40\bootrom.h RTE\Device\TLE9879QXA40\wdt1.h \
+  RTE\Device\TLE9879QXA40\scu.h RTE\Device\TLE9879QXA40\sfr_access.h \
+  RTE\Device\TLE9879QXA40\scu_defines.h RTE\Device\TLE9879QXA40\pmu.h \
+  RTE\Device\TLE9879QXA40\adc1.h RTE\Device\TLE9879QXA40\adc1_defines.h \
+  RTE\Device\TLE9879QXA40\adc2.h RTE\Device\TLE9879QXA40\adc2_defines.h \
+  RTE\Device\TLE9879QXA40\ccu6.h RTE\Device\TLE9879QXA40\ccu6_defines.h \
+  RTE\Device\TLE9879QXA40\int.h RTE\Device\TLE9879QXA40\isr.h \
+  RTE\Device\TLE9879QXA40\lin.h RTE\Device\TLE9879QXA40\port.h \
+  RTE\Device\TLE9879QXA40\cmsis_misra.h RTE\Device\TLE9879QXA40\uart.h \
+  RTE\Device\TLE9879QXA40\uart_defines.h \
+  RTE\Device\TLE9879QXA40\eval_board.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h
